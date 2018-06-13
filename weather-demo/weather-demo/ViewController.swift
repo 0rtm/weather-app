30bdb23d62769dd43fd0a5fc621ae6bc68096571
Artem Tselikov <ar.tselikov@gmail.com>
2018-06-13 10:04:10 -0400

    add empty ios project

---
+++ weather-demo/weather-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-demo
+//
+//  Created by Artem Tselikov on 2018-06-13.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
