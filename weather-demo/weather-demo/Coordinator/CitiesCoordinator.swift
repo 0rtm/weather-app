bc79c583e3ce568b33d0dc9450de12bfdfdc775b
Artem Tselikov <ar.tselikov@gmail.com>
2018-06-13 20:13:44 -0400

    add cities coordinator

---
+++ weather-demo/weather-demo/Coordinator/CitiesCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  CitiesCoordinator.swift
+//  weather-demo
+//
+//  Created by Artem Tselikov on 2018-06-13.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+
+import Foundation
