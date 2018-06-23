95d1f1ea650fca71db382c68ad73286bde7f8517
Artem Tselikov <ar.tselikov@gmail.com>
2018-06-23 19:35:53 -0400

    manually generated file for city object

---
+++ weather-demo/weather-demo/Model/Core Data/City+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  City+CoreDataClass.swift
+//  weather-demo
+//
+//  Created by Artem Tselikov on 2018-06-22.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(City)
+public class City: NSManagedObject {
+
+}
---
+++ weather-demo/weather-demo/Model/Core Data/City+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  City+CoreDataProperties.swift
+//  weather-demo
+//
+//  Created by Artem Tselikov on 2018-06-22.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension City {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<City> {
+        return NSFetchRequest<City>(entityName: "City")
+    }
+
+    @NSManaged public var name: String?
+
+}
